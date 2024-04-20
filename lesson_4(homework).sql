3347844201c7097648b0274b06949dea9bc971db
SARDOR <sardor-mannapov@list.ru>
2024-04-20 15:59:07 +0500

    homework

---
+++ lesson_4(homework).sql
@@ -0,0 +1,10 @@
+--second task
+SELECT quantity_per_unit, units_in_stock
+FROM products
+WHERE quantity_per_unit LIKE '%bottles%' AND units_in_stock < 20;
+
+--third task
+SELECT *
+FROM customers
+JOIN orders ON customers.customer_id = orders.customer_id
+WHERE order_id IS NULL
